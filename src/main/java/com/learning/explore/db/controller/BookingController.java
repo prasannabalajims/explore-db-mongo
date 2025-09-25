f059489f1899021251f7a6d1b1908eb88f16b63c
Prasanna Balaji M S <prasannabalaji.ms@walmart.com>
2025-09-26 02:11:21 +0530

    Added rollback logic for partial failure scenarios

--- src/main/java/com/learning/explore/db/controller/BookingController.java
+++ src/main/java/com/learning/explore/db/controller/BookingController.java
@@ -2,13 +2,11 @@ package com.learning.explore.db.controller;
 
 import com.learning.explore.db.dto.BookingRequest;
 import com.learning.explore.db.service.BookingService;
-import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -19,7 +17,7 @@ public class BookingController {
 
     @PostMapping("/shows/{showId}/book")
     public ResponseEntity<?> book(@PathVariable String showId, @RequestBody BookingRequest req) {
-        String bookingId = bookingService.bookSeats(req.getUserId(), showId, req.getSeatNumbers());
+        String bookingId = bookingService.performSeatReservation(req.getUserId(), showId, req.getSeatNumbers());
         if (bookingId == null) {
             return ResponseEntity.status(HttpStatus.CONFLICT).body(Map.of("error","Seats unavailable"));
         }
--- src/main/java/com/learning/explore/db/service/BookingService.java
+++ src/main/java/com/learning/explore/db/service/BookingService.java
@@ -3,10 +3,7 @@ package com.learning.explore.db.service;
 import com.learning.explore.db.model.Booking;
 import com.learning.explore.db.repository.BookingRepository;
 import com.mongodb.client.MongoCollection;
-import com.mongodb.client.model.Filters;
-import com.mongodb.client.model.FindOneAndUpdateOptions;
-import com.mongodb.client.model.ReturnDocument;
-import com.mongodb.client.model.Updates;
+import com.mongodb.client.model.*;
 import org.bson.Document;
 import org.bson.conversions.Bson;
 import org.bson.types.ObjectId;
@@ -26,13 +23,43 @@ public class BookingService {
     @Autowired
     BookingRepository bookingRepository;
 
-    public String bookSeats(String userId, String showId, List<String> seatNumbers) {
+    public String performSeatReservation(String userId, String showId, List<String> seatNumbers) {
         MongoCollection<Document> collection = mongoTemplate.getCollection("shows");
 
-        List<Bson> elemMatches = seatNumbers.stream()
-                .map(sn -> Filters.elemMatch("seats", Filters.and(Filters.eq("seatNumber", sn), Filters.eq("status", "AVAILABLE"))))
-                .toList();
-        Bson filter = Filters.and(Filters.eq("_id", new ObjectId(showId)), Filters.and(elemMatches));
+        // STEP 1 : Attempt update if the requested Seats are available
+        Document updatedSeats = updateSeat(userId, seatNumbers, showId, collection);
+        if (updatedSeats == null) {
+            // seats were not all available
+            return null;
+        }
+
+        // STEP 2: Post-validate Seats Update
+        boolean allBooked =  validateIfAllSeatsUpdated(userId, seatNumbers, updatedSeats);
+
+        // STEP 3: Rollback (free any seats that were incorrectly booked by this user)
+        if (!allBooked) {
+            rollbackOnPartialSeatsUpdate(collection, userId, seatNumbers, showId);
+            return null; // booking failed
+        }
+
+        // compute amount from updated seats
+        List<Document> seats = (List<Document>) updatedSeats.get("seats");
+        double amount = seatNumbers.stream()
+                .mapToDouble(sn -> seats.stream()
+                        .filter(s -> sn.equals(s.getString("seatNumber")))
+                        .findFirst()
+                        .map(d -> d.getDouble("price"))
+                        .orElse(0.0))
+                .sum();
+
+        Booking booking = new Booking(null, showId, userId, seatNumbers, amount, "CONFIRMED", new Date());
+        booking = bookingRepository.save(booking);
+
+        return booking.getId();
+    }
+
+    private Document updateSeat(String userId, List<String> seatNumbers, String showId, MongoCollection<Document> collection) {
+        Bson filter = Filters.eq("_id", new ObjectId(showId));
 
         Bson update = Updates.combine(
                 Updates.set("seats.$[s].status", "BOOKED"),
@@ -52,26 +79,37 @@ public class BookingService {
                 .arrayFilters(arrayFilters)
                 .returnDocument(ReturnDocument.AFTER);
 
-        Document updated = collection.findOneAndUpdate(filter, update, options);
+        return collection.findOneAndUpdate(filter, update, options);
+    }
 
-        if (updated == null) {
-            // seats were not all available
-            return null;
-        }
+    private boolean validateIfAllSeatsUpdated(String userId, List<String> seatNumbers, Document updatedSeats) {
+        List<Document> seats = (List<Document>) updatedSeats.get("seats");
 
-        // compute amount from updated seats
-        List<Document> seats = (List<Document>) updated.get("seats");
-        double amount = seatNumbers.stream()
-                .mapToDouble(sn -> seats.stream()
-                        .filter(s -> sn.equals(s.getString("seatNumber")))
-                        .findFirst()
-                        .map(d -> d.getDouble("price"))
-                        .orElse(0.0))
-                .sum();
+        return seatNumbers.stream().allMatch(sn ->
+                seats.stream().anyMatch(s ->
+                        sn.equals(s.getString("seatNumber")) &&
+                                "BOOKED".equals(s.getString("status")) &&
+                                userId.equals(s.getString("bookedBy"))
+                )
+        );
+    }
 
-        Booking booking = new Booking(null, showId, userId, seatNumbers, amount, "CONFIRMED", new Date());
-        booking = bookingRepository.save(booking);
+    private void rollbackOnPartialSeatsUpdate(MongoCollection<Document> collection, String userId, List<String> seatNumbers, String showId) {
+        Bson rollbackUpdate = Updates.combine(
+                Updates.set("seats.$[s].status", "AVAILABLE"),
+                Updates.unset("seats.$[s].bookedBy"),
+                Updates.unset("seats.$[s].bookedAt")
+        );
 
-        return booking.getId();
+        List<Bson> rollbackFilters = Collections.singletonList(
+                Filters.and(
+                        Filters.in("s.seatNumber", seatNumbers),
+                        Filters.eq("s.bookedBy", userId) // only rollback seats we just marked
+                )
+        );
+
+        collection.updateOne(Filters.eq("_id", new ObjectId(showId)),
+                rollbackUpdate,
+                new UpdateOptions().arrayFilters(rollbackFilters));
     }
 }
